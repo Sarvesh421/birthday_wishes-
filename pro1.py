d15133fa4bbb6793a2f342212f78293f24592fda
SARVESH R <sr6320@srmist.edu.in>
2024-09-26 22:29:05 +0530

    Add files via upload

---
+++ pro1.py
@@ -0,0 +1,9 @@
+print('\033[91m' + '\n'.join
+      ([''.join
+        ([(' HBD LINGU'[(x-y) % 10]
+           if((x*0.05)**2+(y*0.1)**2-1)
+           **3-(x*0.05)**2*(y*0.1)
+           **3<=0 else' ')
+          for x in range(-30,30)])
+        for y in range(15,-15,-1)]))
+print('\033[0m')
